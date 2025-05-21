5a1b21d1b7e9bccfbba26042a61a1b7d71de32c9
ZaneBennett <150202694+ZaneBennett@users.noreply.github.com>
2025-05-21 15:57:04 -0500

    Add files via upload

---
+++ Visual Basic Example Program/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Items.AddRange(New Object() {"Stan Lee", "Jack Kirby", "Steve Ditko"})
+        ListBox1.Location = New Point(34, 27)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(166, 64)
+        ListBox1.TabIndex = 0
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(77, 107)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 2
+        Button2.Text = "Exit"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(232, 158)
+        Controls.Add(Button2)
+        Controls.Add(ListBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ Visual Basic Example Program/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+        Dim info(3, 3) As String
+
+        info(0, 0) = "Stan Lee"
+        info(0, 1) = "12/28/1922"
+        info(0, 2) = "7065884"
+        info(1, 0) = "Jack Kirby"
+        info(1, 1) = "08/28/1917"
+        info(1, 2) = "6758045"
+        info(2, 0) = "Steve Ditko"
+        info(2, 1) = "11/02/1927"
+        info(2, 2) = "8307368"
+
+        MsgBox("Name: " & info(ListBox1.SelectedIndex, 0) & vbCrLf & "Date of Birth: " & info(ListBox1.SelectedIndex, 1) & vbCrLf & "ID: " & info(ListBox1.SelectedIndex, 2))
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
